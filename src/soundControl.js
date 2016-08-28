d02dc961ba4e5a2df9e580a8619ccd5c0300b4f7
jingjing <yoohyojin@gmail.com>
2016-08-28 03:13:26 -0400

    audio control

--- src/soundControl.js
+++ src/soundControl.js
@@ -1,70 +1,65 @@
-document.getElementById("play_button").addEventListener("click", function() {
-						  audio.play();
-						  this.style.display = "none";
-						  document.getElementById("pause_button").style.display = "inline-block";
-						  document.getElementById("sound").style.opacity = "1";
-						}, false);
+/**
+	// Reference: http://www.html5rocks.com/en/tutorials/speed/animations/
 
-document.getElementById("play_button")
+**/
 
 
-<!-- <audio controls>
-						<source src="audios/lalalaaa.mp3" type="audio/mpeg">
-					</audio>
-					<script type="text/javascript">
-						var button01 = getElementByClass
-					</script> -->
+/**
+	variable to check scrolling and audio status
+
+**/
+var playing = false;
+var audioId;
+var audioList = document.getElementsByTagName("audio");
+
+var last_known_scroll_position = 0;
+var ticking = false;
+
+for (var i = 0; i < audioList.length; i++) {
+    audioList[i].addEventListener("playing", onPlaying, false);
+    audioList[i].addEventListener("playing", onPlaying, false);
+}
 
 
-					<!-- <script type="text/javascript">
-						document.getElementById("play_button").addEventListener("click", function() {
-						  audio.play();
-						  this.style.display = "none";
-						  document.getElementById("pause_button").style.display = "inline-block";
-						  document.getElementById("sound").style.opacity = "1";
-						}, false);
-					</script> -->
+function onPlaying(e) {
+    
+    playing = true;
+    audioId = parseInt(e.target.id);
 
-					
-					<!-- <div> 
-					  <button onclick="document.getElementById('player').play()">Play</button> 
-					  <button onclick="document.getElementById('player').pause()">Pause</button> 
-					</div> -->
+    // console.log(parseInt(audioId) + " playing");
 
-					
-		<!-- VERSION ONE -->
+};
 
-		<!-- <div class="bird_visa one"  data-anchor-target="#interview" data-bottom-top="top:100%;"  data--4p-top="top:3280px;"   data--30p-top="top:280px; display:block;" data--48p-top="top:-200px; ">
-					<audio id="audioOne" src="audios/lalalaaa.mp3" preload></audio>
-					<img class="overlay active" src="images/play-button.png" onclick="document.getElementById('audioOne').play();">
-					<img class="overlay" src="images/stop-button.png" onclick="document.getElementById('audioOne').pause()">
-					<img src="images/bird-visa-01.gif" >
-					
 
-					
-		</div>
+// if (audio.volume !== 1) {
+//         audio.volume = 1;
+//         button.innerHTML = 'change volume to 0.5';
+//     } else {
+//         audio.volume = 0.5;
+//         button.innerHTML = 'change volume to 1';
+//     }
 
-		<div class="rellax bird_visa two" data-anchor-target="#interview" data-bottom-top="top:300%;"  data--20p-top="top:15%;"   data--40p-top="display:block;" data--50p-top="top:15%;" data--68p-top="top:10px;">
-					<audio id="audioTwo" src="audios/high_pitched_hello.mp3" preload></audio>
-					<img class="overlay active" src="images/play-button.png" onclick="document.getElementById('audioTwo').play();">
-					<img class="overlay" src="images/stop-button.png" onclick="document.getElementById('audioTwo').pause()">
-					
-					<img src="images/bird-visa-02.gif" >
-		</div>
+    
+function stopAudio(scroll_pos) {
+	console.log(scroll_pos);
 
-		<div class="bird_visa three" data-anchor-target="#interview" data-bottom-top="top:350%;"  data--55p-top="top:25%;"   data--70p-top="display:block;" data--88p-top="top:-102%; ">
-					<audio id="audioThree" src="audios/ooweee.mp3" preload></audio>
-					<img class="overlay active" src="images/play-button.png" onclick="document.getElementById('audioThree').play();">
-					<img class="overlay" src="images/stop-button.png" onclick="document.getElementById('audioThree').pause()">
-					<img src="images/bird-visa-03.gif" >
-		</div>
+  	if(playing==true){
+  		audioList[+audioId].pause();
+  		window.setTimeout(audioList[+audioId].volume-=0.1, 2000);
+  		playing==false;
+  		console.log(audioList[+audioId].id + "paused");
+  		window.setTimeout(resetAudio(audioId), 2000);
+  	}
+}
 
-		<div class="bird_visa four" data-anchor-target="#interview" data-bottom-top="top:500%;"  data--65p-top="top:15%;"   data--85p-top="display:block;" data--98p-top="top:-106%; ">
-					<audio id="audioFour" src="audios/one_two_three_testing.mp3" preload></audio>
-					<img class="overlay active" src="images/play-button.png" onclick="document.getElementById('audioFour').play();">
-					<img class="overlay" src="images/stop-button.png" onclick="document.getElementById('audioFour').pause()">
-					<img src="images/bird-visa-04.gif" >
-		</div>
-		 -->
-		
-		<!-- VERSION TWO -->
+
+function resetAudio(id){
+	audioList[+id].pause();
+	audioList[+audioId].volume = 1;
+  	audioList[+id].currentTime = 0;
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  stopAudio(last_known_scroll_position);
+});
